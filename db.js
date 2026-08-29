24d451ad8d90947f312050fcd716c94bdf4790a8
Repredador_ <167805848+Repredador@users.noreply.github.com>
2026-08-29 14:11:32 -0400

    Add files via upload

--- db.js
+++ db.js
@@ -686,7 +686,6 @@ Texto a analizar:
 ${textoCrudo}
     `;
 
-    try {
         const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
